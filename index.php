c9ac0e435a85d1bd5f3194d5a23f99e93622dd53
SouravST <86552751+SouravST@users.noreply.github.com>
2021-11-20 23:34:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portfolio</title>
+</head>
+<body><!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content=
+            "width=device-width, initial-scale=1.0">
+        <title>HTML Project</title>
+    </head>
+    
+    <body>
+        <table id="header" border="0" width="100%"
+            cellpadding="0" cellspacing="0" bgcolor="#390879">
+            <tr>
+                <td>
+                    <table border="0" cellpadding="15"
+                        cellspacing="0" width="90%" align="center">
+                        <tr>
+                            <td>
+                                
+    
+                            <td>
+                                <a href="#home" style="text-decoration:none">
+                                    <font face="Verdana" size="5" color=390879>
+                                        Home
+                                    </font>
+                                    
+                                </a>
+                            </td>
+    
+                            <td>
+                                |
+                            </td>
+    
+                            <td>
+                                <a href="#about" style="text-decoration:none">
+                                    <font face="Verdana" size="5" color="yellow">
+                                        About
+                                    </font>
+                                </a>
+                            </td>
+    
+                            <td>
+                                |
+                            </td>
+    
+                            <td>
+                                <a href="#projects" style="text-decoration:none">
+                                    <font face="Verdana" size="5" color=yellow>
+                                        Projects
+                                    </font>
+                                </a>
+                            </td>
+    
+                            <td>
+                                |
+                            </td>
+    
+                            <td>
+                                <a href="#achievements" style="text-decoration:none">
+                                    <font face="Verdana" size="5" color=yellow>
+                                        Achievements
+                                    </font>
+                                </a>
+                            </td>
+    
+                            <td>
+                                |
+                            </td>
+    
+                            <td>
+                                <a href="#contact" style="text-decoration:none">
+                                    <font face="Verdana" size="5" color=yellow>
+                                        Contact
+                                    </font>
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+
+        <table id="home" border="1" width="100%"
+            cellpadding="20" cellspacing="0" bgcolor="grey">
+            <tr>
+                <td>
+                    <table border="0" cellpadding="15"
+                        cellspacing="0" width="90%" align="center">
+                        <tr>
+                            <td align="center" valign="middle">
+                                <h3>
+                                    <font face="Times New Roman"
+                                        size="6" color="#ffffff">
+                                        
+                                    </font>
+                                </h3>
+    
+                                <h2>
+                                    <font face="Verdana" size="6"
+                                        color="b8dflo">
+                                        
+                                    </font>
+                                </h2>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+       
+        <table id="about" border="0" width="100%"
+            cellpadding="0" cellspacing="0" bgcolor="black">
+            <tr>
+                <td>
+                    <table border="0" cellpadding="15"
+                        cellspacing="0" width="80%" align="center">
+                        <tr>
+                            <td height="180" align="center"
+                                valign="middle" colspan="2">
+                                <font face="Verdana" size="7"
+                                    color="390879">
+                                    About Me
+                                </font>
+                                <hr color="" width="90">
+                            </td>
+                        </tr>
+    
+                        <tr>
+                            <td width="40%">
+                                <img src="img.jpg" height="300">
+                            </td>
+    
+                            <td width="60%">
+                                <font face="Verdana" size="4"
+                                    color="white">
+                                   Hii I am Sourav Thakur
+                                    <hr color="black">
+                                    Currently i am pursuing my b.tech in computer science and engineering at GIET University ,GUNUPUR
+                                    <hr color="black">
+    
+                                   I am a front end developer and have knowledge in  c and html language.<div class="i have"></div>
+                                    <hr color="black">
+    
+                                      <hr color="black">
+                                    
+                                </font>
+                            </td>
+                        </tr>
+                    </table>
+                    <hr color="black">
+                    <hr color="black">
+                    <hr color="black">
+                </td>
+            </tr>
+        </table>
+        
+    
+    
+        
+        <table id="projects" border="0" width="100%"
+            cellpadding="0" cellspacing="0" bgcolor="">
+            <tr>
+                <td>
+                    <table border="0" cellpadding="15"
+                        cellspacing="0" width="80%" align="center">
+                        <tr>
+                            <td height="180" align="center"
+                                valign="middle" colspan="2">
+                                <font face="Verdana" size="7"
+                                    color="grey">
+                                    Projects
+                                </font>
+                                
+                            </td>
+                        </tr>
+    
+                        <tr>
+                            <td height="10">
+                                <font face="Times New Roman"
+                                    size="5" color="black">
+                                    <ul>
+                                        <li>
+                                            Login page
+                                            <a href="loginform.html"style="text-decoration:none">
+                                                 ➲
+                                            </a>
+                                        </li>
+    
+                                        <li>
+                                            <hr color="#c2c0c3">
+                                            Signup page
+                                            <a href="signup.html" style="text-decoration:none"
+                                                color="#c2c0c3">
+                                                 ➲
+                                            </a>
+                                        </li>
+                                            
+                                    </ul>
+                                    
+                                </font>
+                            </td>
+    
+                            
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        
+    
+        
+        <table id="achievements" border="0" width="100%"
+            cellpadding="0" cellspacing="0" bgcolor="black">
+            <tr>
+                <td>
+                    <table border="0" cellpadding="15"
+                        cellspacing="0" width="80%" align="center">
+                        <tr>
+                            <td height="180" align="center" valign="middle">
+                                <font face="Verdana" size="7" color="Blue">
+                                    Achievements
+                                    
+                                </font>
+                            </td>
+                        </tr>
+    
+                        <tr>
+                            <td>
+                                <font face="Verdana" size="4" color="white">
+                                    <ul>
+                                        <li>
+                                            <b>Completed 30 days google cloud challenge.</b>
+                                            <img src="cloud.jpeg " width="450"
+                                        </li>
+    
+                                        <li>
+                                            <hr color="black">
+                                            <hr color="black">
+                                            <hr color="black">
+                                            <b>Completed c programming language from iit bombay.</b>
+                                 <img src="iit.jpeg" width="450"
+                                        </ul>
+    
+                                </font>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>        
+        <table id="contact" border="0" width="100%"
+            cellpadding="0" cellspacing="0" bgcolor="#c2c0c3">
+            <tr>
+                <td>
+                    <table border="0" cellpadding="15"
+                        cellspacing="0" width="80%" align="center">
+                        <tr>
+                            <td height="180" align="center"
+                                valign="middle" colspan="2">
+                                <font face="Verdana" size="7"
+                                    color="black">
+                                    Contact
+                                </font>
+                                <hr color="black" width="90">
+                            </td>
+                        </tr>
+    
+                        <tr>
+                            <td align="center" valign="top">
+                                <table border="0" width="50%" cellpadding="15"
+                                    cellspacing="0" align="center" bgcolor="black">
+                                    <tr>
+                                        <td width="30%">
+                                            <hr color="black">
+                                            <font face="Verdana" size="4"
+                                                color="#ffffff">
+                                                Name: 
+                                            </font>
+                                        </td>
+                                        <td width="70%">
+                                            <font face="Verdana" size="4"
+                                                color="#ffffff"><br>
+                                                Sourav Thakur
+                                            
+                                            </font>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td width="30%">
+                                            <font face="Verdana" size="4"
+                                                color="#ffffff">
+                                                Email:
+                                            </font>
+                                        </td>
+                                        <td width="70%">
+                                            <font face="Verdana" size="4"
+                                                color="#ffffff">
+                                               thakursouravst02@gmail.com
+                                            </font>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                       
+                                       
+                                    </tr>
+                                    <tr>
+                                        
+                                      
+                                    </tr>
+                                    <tr>
+                                        <td width="30%">
+                                            
+                                        </td>
+                                        <td width="70%">
+                                            <button type="Submit">
+                                                <font face="Verdana"
+                                                    size="4" color="black">
+                                                    <b>Submit</b>
+                                                </font>
+                                            </button>
+                                            <hr color="black">
+                                            <hr color="black">
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+    
+                        </tr>
+                        <tr>
+                            <td colspan="2">   
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+</body>
+</html>
